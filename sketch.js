e07b859919b3ce90c0412f8887aca25316cb2d4f
DaRealIfrit <68937928+DaRealIfrit@users.noreply.github.com>
2021-02-16 08:13:01 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,32 +5,42 @@ function setup() {
 
   speed = random(223, 321);
   weight = random(30, 50);
-  thickness = random(22,83)
+  thickness = random(22,83);
 
   bullet = createSprite(50, 200, 50, 10);
   bullet.shapeColor = color(255)
   bullet.velocityX = speed;
 
   wall = createSprite(1400, 200, thickness, height/2);
-  wall.shapeColor = color(80, 80, 80)
-
+  wall.shapeColor = color(80, 80, 80);
 }
 
 function draw() {
   background(0);
 
+  if(keyDown("r")){
+    speed = random(223, 321);
+    weight = random(30, 50);
+    thickness = random(22,83);
+    bullet.shapeColor = color(255);
+    bullet.velocityX = speed;
+    bullet.x = 200;
+    wall.width = thickness;
+
+  }
+  
   if(wall.x - bullet.x < (bullet.width + wall.width) /2){
     bullet.velocityX = 0;
     bullet.velocityX = 0;
-    var thicc = thickness*thickness*thickness;
-    var damage = 0.5 * weight * speed * speed/thicc;
+    var thikc = thickness*thickness*thickness;
+    var damage = 0.5 * weight * speed * speed/thikc;
   }
 //10648
 
     stroke("white");
     textSize(20);
     fill("white");
-    text("THICC: " + thicc, 100, 80);
+    text("Thickness: " + thikc, 100, 80);
 
     stroke("white");
     textSize(20);
@@ -45,8 +55,4 @@ function draw() {
     wall.shapeColor = color(0, 255, 0);
   }
   drawSprites();
-}
-
-function hasCollided(lbullet, lwall){
-  bulletRigthEdge
 }
